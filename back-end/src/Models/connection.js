d6a4b7a24ac65ee13ea65ba4fd6478f672b5308c
lucas-paz <lucas.hpaz@outlook.com>
2021-11-03 14:14:52 -0300

    fix: pass DB nato to camel case

--- back-end/src/Models/connection.js
+++ back-end/src/Models/connection.js
@@ -2,7 +2,7 @@ const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
 const MONGO_DB_URL = process.env.MONGO_DB_URL
-const DB_NAME = 'trybe_tech_blitz';
+const DB_NAME = 'trybeTechBlitz';
 
 let schema = null;
 
