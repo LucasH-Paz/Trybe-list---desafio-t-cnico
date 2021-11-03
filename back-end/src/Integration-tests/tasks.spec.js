18508ef3d861c86861ee5b1bb80e1f80ccf5edfd
lucas-paz <lucas.hpaz@outlook.com>
2021-11-03 18:44:05 -0300

    feat: tests for the addNew route

--- back-end/src/Integration-tests/tasks.spec.js
+++ back-end/src/Integration-tests/tasks.spec.js
@@ -21,25 +21,108 @@ const DEFAULT_UPLOAD = {
 };
 
 describe('Novas tarefas', () => {
-  it('É possível adicionar novas tarefas', () => {});
-  it('Não é possível adicionar novas tarefas sem titulo', () => {});
-  it('Não é possível adicionar novas tarefas sem descrição', () => {});
-  it('Não é possível adicionar novas tarefas sem status', () => {});
+  before(async () => {
+    const VirtualDB = await getConnection();
+    sinon.stub(MongoClient, 'connect').resolves(VirtualDB);
+  });
+
+  after(() => {
+    MongoClient.connect.restore();
+  });
+
+
+  it('É possível adicionar novas tarefas', async () => {
+    const response = await chai.request(server).post('/tasks').send(DEFAULT_PAYLOAD);
+
+    expect(response).to.have.status(201);
+    expect(response).to.be.a('object');
+    expect(response.body).to.have.property('result');
+    expect(response.body.result).to.have.property('title');
+  });
+
+  it('Não é possível adicionar novas tarefas sem titulo', async () => {
+    const { title, ...withoutKey } = DEFAULT_PAYLOAD;
+    const response = await chai.request(server).post('/tasks').send(withoutKey);
+
+    expect(response).to.have.status(400);
+    expect(response.body).to.have.property('message');
+    expect(response.body.message).to.be.equal('Invalid entries. Try again.');
+  });
+
+  it('Não é possível adicionar novas tarefas sem descrição', async () => {
+    const { description, ...withoutKey } = DEFAULT_PAYLOAD;
+    const response = await chai.request(server).post('/tasks').send(withoutKey);
+
+    expect(response).to.have.status(400);
+    expect(response.body).to.have.property('message');
+    expect(response.body.message).to.be.equal('Invalid entries. Try again.');
+  });
+
+  it('Não é possível adicionar novas tarefas sem status', async () => {
+    const { status, ...withoutKey } = DEFAULT_PAYLOAD;
+    const response = await chai.request(server).post('/tasks').send(withoutKey);
+
+    expect(response).to.have.status(400);
+    expect(response.body).to.have.property('message');
+    expect(response.body.message).to.be.equal('Invalid entries. Try again.');
+  });
 });
 
-describe('Atualizar tarefas', () => {
-  it('É possível atualizar uma tarefa', () => {});
-  it('Não é possível atualizar tarefas sem titulo', () => {});
-  it('Não é possível atualizar tarefas sem descrição', () => {});
-  it('Não é possível atualizar tarefas sem status', () => {});
-  it('Não é possível atualizar tarefas com id inválido', () => {});
+describe.only('Atualizar tarefas', () => {
+  before(async () => {
+    const VirtualDB = await getConnection();
+    sinon.stub(MongoClient, 'connect').resolves(VirtualDB);
+  });
+
+  after(() => {
+    MongoClient.connect.restore();
+  });
+
+  it('É possível atualizar uma tarefa', async () => {
+    const { body: { result: { id } } } = await chai.request(server).post('/tasks').send(DEFAULT_PAYLOAD);
+    console.log(id);
+    const response = await chai.request(server).put(`/tasks/${id}`).send(DEFAULT_UPLOAD);
+
+    expect(response).to.have.status(201);
+    expect(response).to.be.a('object');
+    expect(response.body).to.have.property('result');
+    expect(response.body.result).to.have.property('title');
+    expect(response.body.result.title).to.be.equal(DEFAULT_UPLOAD.title);
+  });
+
+  // it('Não é possível atualizar tarefas sem titulo', () => {});
+
+  // it('Não é possível atualizar tarefas sem descrição', () => {});
+
+  // it('Não é possível atualizar tarefas sem status', () => {});
+
+  // it('Não é possível atualizar tarefas com id inválido', () => {});
 });
 
 describe('Deletar tarefas', () => {
+  before(async () => {
+    const VirtualDB = await getConnection();
+    sinon.stub(MongoClient, 'connect').resolves(VirtualDB);
+  });
+
+  after(() => {
+    MongoClient.connect.restore();
+  });
+
   it('É possível Deletar uma tarefa', () => {});
+
   it('Não é possível deletar uma tarefa com id inválido', () => {});
 });
 
 describe('Listar todas as tarefas', () => {
+  before(async () => {
+    const VirtualDB = await getConnection();
+    sinon.stub(MongoClient, 'connect').resolves(VirtualDB);
+  });
+
+  after(() => {
+    MongoClient.connect.restore();
+  });
+
   it('É possível listar todas as tarefas', () => {});
 });
